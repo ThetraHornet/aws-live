33e9605a380dde8c83329adbaaf0a46f4392257b
ThetraHornet <45752675+ThetraHornet@users.noreply.github.com>
2024-12-07 22:11:17 +0800

    config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "khenghui-employee"
 customregion = "us-east-1"
 
